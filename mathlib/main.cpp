ad6152dbc4d08571775a19268bdf7adbe255ec84
PYZ <592744797@qq.com>
2019-10-07 13:17:20 +0800

    [feature]:新增matrix

--- mathlib/main.cpp
+++ mathlib/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-
+#include "matrix.h"
 int main()
 {
-	printf("hi\n");
+	matrix mat;
+	init_matrix(mat,4,4);
+	float r0[] = {1,1,1,1};
+	input_matrix_row(mat, 0, r0, sizeof(r0) / sizeof(float));
+	print_matrix(mat);
+	getchar();
+	release_matrix(mat);
 	return 0;
 }
