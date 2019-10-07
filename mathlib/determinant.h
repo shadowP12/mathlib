98de9aaac7f48274199139964793bc4b0ceb264d
PYZ <592744797@qq.com>
2019-10-07 19:26:02 +0800

    [feature]:添加求逆序数和余子式

--- mathlib/determinant.h
+++ mathlib/determinant.h
@@ -1,3 +1,50 @@
 #pragma once
 #include "base.h"
 
+//求逆序数
+inline int number_inversions(int* data, int length)
+{
+	int res = 0;
+	for (int i = 0; i < length; i++)
+	{
+		int cur = data[i];
+		for (int j = 0; j < i; j++)
+		{
+			int pre = data[j];
+			if (pre > cur)
+				res++;
+		}
+	}
+	return res;
+}
+
+//求余子式
+inline BaseData cofactor(BaseData& base_data, int cur_row, int cur_col)
+{
+	BaseData res;
+	init_base_data(res, base_data.row - 1, base_data.col - 1);
+	for (int i = 0; i < base_data.row; i++)
+	{
+		for (int j = 0; j < base_data.col; j++)
+		{
+			if (i == cur_row - 1 || j == cur_col - 1)
+			{
+			}
+			else if (i > cur_row - 1 || j > cur_col - 1)
+			{
+				int r = i, c = j;
+				if (i > cur_row - 1)
+					r = i - 1;
+				if (j > cur_col - 1)
+					c = j - 1;
+				res.data[r][c] = base_data.data[i][j];
+			}
+			else
+			{
+				res.data[i][j] = base_data.data[i][j];
+			}
+		}
+	}
+	return res;
+}
+
--- mathlib/main.cpp
+++ mathlib/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include "matrix.h"
+#include "determinant.h"
 int main()
 {
 	BaseData data;
-	init_base_data(data,4,4);
-	float r0[] = {1,1,1,1};
+	init_base_data(data, 4, 4);
+	float r0[] = { 1,6,1,1 };
+	float r1[] = { 3,1,7,1 };
+	float r2[] = { 0,6,0,4 };
+	float r3[] = { 9,1,3,1 };
 	input_base_data_row(data, 0, r0, sizeof(r0) / sizeof(float));
+	input_base_data_row(data, 1, r1, sizeof(r1) / sizeof(float));
+	input_base_data_row(data, 2, r2, sizeof(r2) / sizeof(float));
+	input_base_data_row(data, 3, r3, sizeof(r3) / sizeof(float));
 	print_base_data(data);
-	getchar();
+	BaseData data_1 = cofactor(data,1,2);
+	printf("\n");
+	print_base_data(data_1);
 	release_base_data(data);
+	release_base_data(data_1);
+	getchar();
+	
 	return 0;
 }
