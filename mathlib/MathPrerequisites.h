1d11eb548bd0a2b7734df5451f6d0fb13c0c8fcc
pan <592744797@qq.com>
2022-05-02 18:16:02 +0800

    [feature]:清理非必要文件

--- mathlib/MathPrerequisites.h
+++ mathlib/MathPrerequisites.h
@@ -1,21 +1,16 @@
 #pragma once
 #include <cmath>
-// std::sin,std::cos都以弧度为单位
 #define PI 3.1415926
-// 角度转弧度
-inline float radians(float degrees)
-{
+
+float Radians(float degrees) {
 	return (PI / 180.0f) * degrees;
 }
 
-// 弧度转角度
-inline float degrees(float radians)
-{
+float Degrees(float radians) {
 	return (radians * 180.0) / PI;
 }
 
-inline void swap(float& a, float& b)
-{
+void Swap(float& a, float& b) {
 	float t = a;
 	a = b;
 	b = t;
